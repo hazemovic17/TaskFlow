0b3571799ba44a3692f0f6fc43242546f778aae7
hazemovic17 <102318285+hazemovic17@users.noreply.github.com>
2026-08-31 18:26:21 +0300

    Add project files.

---
+++ TaskFlow.Domain/Entities/TaskItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Entities
+{
+    
+    public class TaskItem
+    {
+        public Guid Id { get; set; } //ID is a globaly unique identifier for the Task that lowers the chance of conflict and ID duplication
+
+        public string Title { get; set; } = string.Empty;// title of Task is not null but will be empty text 
+
+        public string? Description { get; set; } //Descritption of task could be nullable
+
+        public bool IsDone { get; set; } = false; //status of the task if it is done or not
+
+        public DateTime? DueDate { get; set; } //end date of the task and it could be nullable which means this task could be done at anytime
+
+        public Guid AssignedUserId { get; set; } //the id of the assigned user in this task
+                                                 //foregin key  of the user.Id
+        public User AssignedUser { get; set; } = null!; //the user itslef the task is assigned to (navigation property)
+    }
+}
---
+++ TaskFlow.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Enums;
+
+namespace TaskFlow.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } //ID is a globaly unique identifier for the user that lowers the chance of conflict and ID duplication
+
+        public string Name { get; set; } = string.Empty; //Name of the user is not null but will be empty text 
+
+        public string Email { get; set; } = string.Empty;//email is not null but will be empty text 
+
+        public string Password { get; set; } = string.Empty;//Password is not null but will be empty text 
+
+        public UserRole Role { get; set; } // which role the user would be (Navigation Property)
+    }
+}
