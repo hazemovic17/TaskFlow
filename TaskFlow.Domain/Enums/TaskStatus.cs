5293d1db2262abbd48f0311e659189312ee226fa
hazemovic17 <102318285+hazemovic17@users.noreply.github.com>
2026-09-01 15:22:49 +0300

    TaskFlowApplication layer

---
+++ TaskFlow.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Enums;
+
+public enum TaskStatus
+{
+    Pending, //task is assigned but the user hadn't started yet
+    InProgress,//the user has already been working on it
+    Completed // done
+}
