629bf34d07af0c0aad5120b1a55c9b0df13d4714
hazemovic17 <102318285+hazemovic17@users.noreply.github.com>
2026-09-01 15:22:05 +0300

    Initial commitAdd project files.

---
+++ TaskFlow.Application/DTO/Auth/LoginRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Application.DTO.Auth
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ TaskFlow.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id); //get user by ID
+
+        Task<User?> GetByEmailAsync(string email);//get uuser by their email
+
+        Task AddAsync(User user); //adds a new user
+    }
+}
