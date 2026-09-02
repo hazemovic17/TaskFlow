b96c0671641eaae9e0dc66c2378a651b69a0e91a
hazemovic17 <102318285+hazemovic17@users.noreply.github.com>
2026-09-02 17:50:19 +0300

    chagning AuthService into usecases and dealing with them directly without interfaces

---
+++ TaskFlow.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskFlow.Application.Interfaces.Repositories;
+using TaskFlow.Application.UseCases;
+using TaskFlow.Infrastructure.Persistence;
+using TaskFlow.Infrastructure.Repositories;
+
+namespace TaskFlow.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<TaskFlowDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ITaskRepository, TaskRepository>();
+
+        return services;
+    }
+
+  
+
+
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<LoginUseCase>();
+        services.AddScoped<RegisterUseCase>();
+
+        return services;
+    }
+}
